298456ad859f2a6b05b8462d69b979c23087177d
_ssep_ <99297374+ssepsteve@users.noreply.github.com>
2024-10-29 09:41:39 -0500

    Añade Quimica

--- src/js/main.js
+++ src/js/main.js
@@ -39,7 +39,9 @@ const scores = {
       'Eléctrica': 'La Ingeniería Eléctrica estudia y aplica la energía eléctrica en diversos campos, desde redes de energía hasta energías renovables.',
       'Electrónica': 'La Ingeniería Electrónica se centra en el diseño y aplicación de dispositivos electrónicos, como sistemas de comunicación y control.',
       'Mecánica': 'La Ingeniería Mecánica diseña y mejora maquinaria y sistemas mecánicos, con aplicaciones en múltiples industrias.',
-      'Mecatrónica': 'La Ingeniería Mecatrónica integra mecánica, electrónica y computación para crear sistemas automatizados y robóticos.'
+      'Mecatrónica': 'La Ingeniería Mecatrónica integra mecánica, electrónica y computación para crear sistemas automatizados y robóticos.',
+      'Quimica': 'La Ingeniería Química desarrolla procesos sostenibles y nuevos materiales para industrias como la farmacéutica y alimentaria.'
+      
     };
 
     if (recommendedCareer) {
